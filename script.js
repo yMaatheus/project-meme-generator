709766ad06e37fe4e07d8331bb019a6b0b3fc49f
Matheus <stvgamer74@gmail.com>
2022-04-02 14:33:54 -0300

    Requisitos bonus feitos

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const inputText = document.querySelector('#text-input');
 const inputImage = document.querySelector('#meme-insert');
+const fire = document.querySelector('#fire');
+const water = document.querySelector('#water');
+const earth = document.querySelector('#earth');
+const meme = document.querySelectorAll('.meme');
 
 function onInputTextKeyUpEvent(event) {
   const text = document.querySelector('#meme-text');
@@ -18,9 +22,34 @@ function onInputImageChangeEvent(event) {
   memeImage.src = url;
 }
 
+function onChangeBorderContainer({ target: { id } }) {
+  const memeImageContainer = document.querySelector('#meme-image-container');
+  let border;
+  if (id === 'fire') {
+    border = '3px dashed red';
+  } else if (id === 'water') {
+    border = '5px double blue';
+  } else if (id === 'earth') {
+    border = '6px groove green';
+  }
+  memeImageContainer.style.border = border;
+}
+
+function onClickChangeMemeImageEvent({ target: { id } }) {
+  const memeImage = document.querySelector('#meme-image');
+  memeImage.src = `/imgs/${id.replace('-', '')}.png`;
+}
+
 function loadListeners() {
   inputText.addEventListener('keyup', onInputTextKeyUpEvent);
   inputImage.addEventListener('change', onInputImageChangeEvent);
+  fire.addEventListener('click', onChangeBorderContainer);
+  water.addEventListener('click', onChangeBorderContainer);
+  earth.addEventListener('click', onChangeBorderContainer);
+  for (let index = 0; index < meme.length; index += 1) {
+    const element = meme[index];
+    element.addEventListener('click', onClickChangeMemeImageEvent);
+  }
 }
 
 window.onload = () => {
